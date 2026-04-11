6455f894fda687289f3bcfb52a44c35438eb7d41
Cherukuri Vastav Sekar <2024csm.r58@svce.edu.in>
2026-04-11 18:21:39 +0530

    Delete js directory

--- js/supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://kfzascdgwoehkitzhkhw.supabase.co";
-
-const SUPABASE_KEY = "sb_publishable_8N9GLSSOooKWjJInZUE9-A_xEUzNvX3";
-
-window.supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
